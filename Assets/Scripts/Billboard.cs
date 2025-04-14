50cb74a694b3b1db01ef09985ce6446d0637dd55
Chris-Kindred <ckindred@pratt.edu>
2025-04-14 19:00:29 -0400

    I changed so much lemme tell ya
    Added:
    - A functional ladder object and accompanying script
    - Billboarding for 2D sprites
    - Demonstrated a level layout via foreground, middleground, and background principles
    - Added a rotation script for the sky
    - Goku

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [SerializeField] bool freezeXZAxis = true;
+    void Update()
+    {
+        if (freezeXZAxis)
+        {
+            transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        }
+        else
+        {
+            transform.rotation = Camera.main.transform.rotation;
+        }
+    }
+}
